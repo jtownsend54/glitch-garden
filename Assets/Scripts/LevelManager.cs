e34891dd1bf4db1ad63771410435c2004209caa7
Jon Townsend <jon.townsend@limelyte.com>
2017-04-07 10:05:46 -0700

    Updated spalsh screen and start screen to scale better
    Changed the font for any text on screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string name) {
+		Debug.Log("Load the level: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log("Quit request.");
+		Application.Quit();
+	}
+}
---
+++ Assets/Splash/SplashScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad >= 3) {
+			LevelManager manager = GameObject.Find ("LevelManager").GetComponent<LevelManager>() as LevelManager;
+			manager.LoadNextLevel();
+		}
+	}
+}
