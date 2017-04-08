8eee00edcf0d31459d32f6bc1a8987c96fbc7678
Jon Townsend <jon.townsend@limelyte.com>
2017-04-07 22:16:17 -0700

    Added a basic player prefs proxy class

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+	const string VOLUME = "volume";
+	const string DIFFICULTY = "difficulty";
+
+	public static void set(string key, float value) {
+		PlayerPrefs.SetFloat (key, value);
+	}
+
+	public static void set(string key, string value) {
+		PlayerPrefs.SetString (key, value);
+	}
+
+	public static void set(string key, int value) {
+		PlayerPrefs.SetInt (key, value);
+	}
+
+	public static float get(string key, float value) {
+		return PlayerPrefs.GetFloat (key);
+	}
+	
+	public static string get(string key, string value) {
+		return PlayerPrefs.GetString (key);
+	}
+	
+	public static int get(string key, int value) {
+		return PlayerPrefs.GetInt (key);
+	}
+}
