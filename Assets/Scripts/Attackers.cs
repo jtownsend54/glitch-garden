58fb8ce5d16f1706423e9ca62a172ff30d03583a
Jon Townsend <jon.townsend@limelyte.com>
2017-05-13 21:47:42 -0700

    Attackers movement
    *Added script to use for managing movement.
    *Walking speed property is used to update its translate value.

---
+++ Assets/Scripts/Attackers.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attackers : MonoBehaviour {
+
+	[Range(-1, 1)]
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
