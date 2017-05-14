e37d7ada56f1dd8af1601c47b96625b8ba598f1e
Jon Townsend <jon.townsend@limelyte.com>
2017-05-14 06:51:37 -0700

    Added colliders to all the game objects

--- Assets/Defenders.cs
+++ Assets/Defenders.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Zucchini : MonoBehaviour {
+public class Defenders : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,5 +11,9 @@ public class Zucchini : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log (gameObject + " collided with " + collision.gameObject);
 	}
 }
--- Assets/Scripts/Attackers.cs
+++ Assets/Scripts/Attackers.cs
@@ -8,11 +8,16 @@ public class Attackers : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Rigidbody2D body = gameObject.AddComponent<Rigidbody2D> ();
+		body.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log (gameObject + " collided with " + collider.gameObject);
 	}
 }
