0998246663fd4372c618cd677610946ea107c516
Jon Townsend <jon.townsend@limelyte.com>
2017-07-01 08:24:26 -0700

    Added shooter script and gun object
    Shooter includes function for animator to call that generates a projectile at the position of the Gun object.
    The gun object is just an empty object that is a part of the defender that sits where a projectile should be spawned from.

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * This class has a projectile and shoots (moves) it.
+ */
+public class Shooter : MonoBehaviour {
+	public GameObject projectile;
+	public GameObject projectileParent;
+
+	public void Fire() {
+		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		newProjectile.transform.position = gameObject.transform.Find("Gun").position;
+	}
+}
