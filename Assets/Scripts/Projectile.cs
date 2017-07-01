2cd551d0a82317e85deec8392342caaaba4af8dd
Jon Townsend <jon.townsend@limelyte.com>
2017-07-01 07:27:29 -0700

    Moved projectiles out of the parent objects
    *Gave them there own animators
    *Setup mirrors others. Parent object with script, body, and animator. Child body with sprite renderer.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
+	public float damage = 25f;
+	public float speed = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,10 +12,18 @@ public class Projectile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		gameObject.transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
-		Debug.Log (gameObject + " collided with " + collision.gameObject);
+		Debug.Log (collision.gameObject);
+		Health health = collision.gameObject.GetComponent<Health> ();
+
+		health.DealDamage (damage);
+
+		// Destroy the attacker
+		if (health.health <= damage) {
+			Destroy(collision.gameObject);
+		}
 	}
 }
