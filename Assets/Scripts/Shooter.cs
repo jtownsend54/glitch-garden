0a2837932da44aa9e1a252813349225d14af4eb7
Jon Townsend <jon.townsend@limelyte.com>
2017-07-13 06:18:05 -0700

    Added Start method to shooter to create the projectileParent object
    *Caused error when trying to use it during the fire method.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,13 +6,26 @@ using System.Collections;
  */
 public class Shooter : MonoBehaviour {
 	public GameObject projectile;
-	public GameObject projectileParent;
+
+	private GameObject projectileParent;
+		
+	// Need to create the projectile parent on start or it may not exist during the fire method. (Video #165).
+	void Start () {
+		projectileParent = GameObject.Find ("Projectiles");
+			
+		if (!projectileParent) {
+			projectileParent = new GameObject("Projectiles");
+		}
+	}
 
 	public void Fire() {
 		GameObject newProjectile = Instantiate (projectile) as GameObject;
+		// Make sure any spawned projectiles go under the Projectiles object
+		// Setting the parent of a transform which resides in a prefab is disabled to prevent data corruption.
+		newProjectile.transform.parent = projectileParent.gameObject.transform;
+		//newProjectile.transform.position
 		newProjectile.transform.position = gameObject.transform.Find("Gun").position;
 
-		// Make sure any spawned projectiles go under the Projectiles object
-		newProjectile.transform.SetParent (projectileParent.transform);
+
 	}
 }
