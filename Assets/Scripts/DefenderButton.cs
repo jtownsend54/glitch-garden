99db228960b004cf81f7f8425d4220a58af46053
Jon Townsend <jon.townsend@limelyte.com>
2017-08-29 08:46:26 -0700

    Added cost text to the defender buttons

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DefenderButton : MonoBehaviour {
 	public static GameObject defender;
 	public GameObject prefab;
+	private Text defenderCost;
 
 	private DefenderButton[] buttons;
 
@@ -14,6 +16,16 @@ public class DefenderButton : MonoBehaviour {
 
 		// Create an array of buttons so we can loop through them later
 		buttons = GameObject.FindObjectsOfType<DefenderButton> ();
+
+		// Get the UI text element from the defender button
+		defenderCost = gameObject.GetComponentInChildren<Text> ();
+
+		// Get the star cost from the game prefab
+		float cost = prefab.gameObject.GetComponent<Defenders> ().starCost;
+
+		// Set the star cost
+		defenderCost.text = cost.ToString ();
+
 	}
 	
 	// Update is called once per frame
